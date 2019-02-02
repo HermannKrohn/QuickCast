4b1885a9bf12c3ed75863ed970c7326dac96ce90
Hermann Krohn <hermannhumberto@gmail.com>
2019-02-02 16:45:50 -0500

    update

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Express session
 app.use(session({
   secret: 'secret',
