09d6f27f95e0354c9d824956a739919fc40a3e8e
Hermann Krohn <hermannhumberto@gmail.com>
2019-02-02 16:24:14 -0500

    pushing files

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var session = require('express-session');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+//BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//Express session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use('/', routes);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+  console.log('Server started on port ' + app.get('port'));
+});
