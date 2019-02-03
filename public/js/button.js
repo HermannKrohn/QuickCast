13b770c6fd21e2af4c5296813ebc4e0ec2b68af9
Hermann Krohn <hermannhumberto@gmail.com>
2019-02-02 19:01:38 -0500

    first verion with button working smoothly

--- public/js/button.js
+++ public/js/button.js
@@ -1,17 +1,23 @@
-const realFileBtn = document.getElementById("real-file");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
-console.log('In button js file');
+window.onload = function(){
+  const realFileBtn = document.getElementById("real-file");
+  const customBtn = document.getElementById("custom-button");
+  const customTxt = document.getElementById("custom-text");
+  console.log('In button js file');
 
-customBtn.addEventListener("click",function(){
-  realFileBtn.click();
-  console.log('CLICK');
-});
-
-realFileBtn.addEventListener("change",function(){
-  if (realFileBtn.value){
-    customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-  }else{
-    customTxt.innerHTML = "No File Uploaded";
+  if(customBtn){
+    customBtn.addEventListener("click",function(){
+      realFileBtn.click();
+      console.log('CLICK');
+    });
   }
-});
+
+  if(realFileBtn){
+    realFileBtn.addEventListener("change",function(){
+      if (realFileBtn.value){
+        customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+      }else{
+        customTxt.innerHTML = "No File Uploaded";
+      }
+    });
+  }
+}
